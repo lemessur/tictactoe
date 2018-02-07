4e2aebc89e379bc0bceba70840604175827ae8df
Addison LeMessurier <lemessur@gmail.com>
2018-02-07 08:58:55 -0700

    Added win checking

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -12,15 +12,18 @@ export default class Board extends React.Component {
 
         this.state = {
             squares: getEmptyBoard(),
-            nextTurn: 'x'
+            nextTurn: 'x',
+            win: undefined      // the player who has won
         };
 
         this.handleClick = this.handleClick.bind(this);
         this.handleReset = this.handleReset.bind(this);
+        this.getStatus = this.getStatus.bind(this);
+        this.checkWin = this.checkWin.bind(this);
     }
 
     handleClick(index) {
-        if (this.state.squares[index] === emptySquare) {
+        if (!this.state.win && this.state.squares[index] === emptySquare) {
             let nextTurn = this.state.nextTurn;
             let squares = this.state.squares;
 
@@ -31,10 +34,13 @@ export default class Board extends React.Component {
                 nextTurn = 'x';
             }
 
+            const win = this.checkWin();
+
             this.setState(() => {
                 return {
                     squares,
-                    nextTurn
+                    nextTurn,
+                    win
                 };
             });
         }
@@ -44,20 +50,98 @@ export default class Board extends React.Component {
         this.setState(() => {
             return {
                 squares: getEmptyBoard(),
-                nextTurn: firstTurn
+                nextTurn: firstTurn,
+                win: undefined
             };
         });
     }
 
-    // checkWin() {
-    //     // Check rows
-    //     let value = emptySquare;
-    //     for (y = 0; y < boardSize; ++y) {
+    checkWin() {
+        // Check rows
+        for (let y = 0; y < (boardSize * boardSize); y += boardSize) {
+            let value = this.state.squares[y];
+            if (value !== emptySquare) {
+                let foundLine = true;
 
-    //     }
-    // }
+                // Start from the left
+                for (let x = y; x < y + boardSize; ++x) {
+                    if (this.state.squares[x] !== value) {
+                        foundLine = false;
+                        break;
+                    }
+                }
+
+                if (foundLine) {
+                    // Report horizontal win
+                    return value;
+                }
+            }
+        }
+
+        // Check columns
+        for (let x = 0; x < boardSize; ++x) {
+            let value = this.state.squares[x];
+            if (value !== emptySquare) {
+                let foundLine = true;
+
+                // Start from the top
+                for (let y = x; y < (boardSize * boardSize); y += boardSize) {
+                    if (this.state.squares[y] !== value) {
+                        foundLine = false;
+                        break;
+                    }
+                }
+
+                if (foundLine) {
+                    // Report vertical win
+                    return value;
+                }
+            }
+        }
+
+        // Check left diagonal
+        let value = this.state.squares[0];      // Start from the top left
+        if (value !== emptySquare) {
+            let foundLine = true;
+
+            for(let i = 0; i < (boardSize * boardSize); i += (boardSize + 1)) {
+                if (this.state.squares[i] !== value) {
+                    foundLine = false;
+                    break;
+                }
+            }
+
+            if (foundLine) {
+                // Report left diagonal win
+                return value;
+            }
+        }
+
+        // Check right diagonal
+        value = this.state.squares[boardSize - 1];      // Start from the top right
+        if (value !== emptySquare) {
+            let foundLine = true;
+
+            for(let i = boardSize - 1; i < ((boardSize * boardSize) - 1); i += (boardSize - 1)) {
+                console.log(i);
+                if (this.state.squares[i] !== value) {
+                    foundLine = false;
+                    break;
+                }
+            }
+
+            if (foundLine) {
+                // Report right diagonal win
+                return value;
+            }
+        }
+    }
 
     getStatus() {
+        if (this.state.win) {
+            return "Player " + this.state.win + " wins!";
+        }
+
         return "Next turn: " + this.state.nextTurn;
     }
 
